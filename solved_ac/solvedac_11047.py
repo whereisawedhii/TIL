422e60fb1bfb48b567dbac78cbf83684fac90faf
문재성 <zsm1230@gmail.com>
2024-01-23 14:26:45 +0900

    unsolved_ac

---
+++ solved_ac/solvedac_11047.py
@@ -0,0 +1,31 @@
+# import sys
+# sys.stdin = open('input.txt', 'r')
+
+
+# N, money = map(int, sys.stdin.readline().split())
+# coins= [int(sys.stdin.readline().strip()) for i in range(N)]
+
+# coins.sort(reverse=True)
+# counts = 0
+
+# for i in coins : 
+#     if i < money : 
+#         counts += money // i
+#         money = money % i
+
+# print(counts)
+
+
+N, money = map(int, input().split())
+coins= [int(input().strip()) for i in range(N)]
+coins.sort(reverse=True)
+counts = 0
+
+for i in coins : 
+    if money == 0 :
+        break
+    elif i < money : 
+        counts += money // i
+        money = money % i
+
+print(counts)
